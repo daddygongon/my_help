b827936b5014a84c7779b96df8979d7561d2e418
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2016-12-01 19:09:04 +0900

    change + -> -

--- lib/specific_help.rb
+++ lib/specific_help.rb
@@ -155,7 +155,7 @@ module SpecificHelp
       print_separater
       items =@help_cont[key_word]
 #      puts items[:title]
-      puts CodeRay.scan("*#{items[:title]}:", :Taskpaper).term
+      puts CodeRay.scan("-#{items[:title]}:", :Taskpaper).term
       disp(items[:cont])
       print_separater
     end
