c0584ca6a94a4b708f0d95a15656d9f304be4772
Toshiharu Oyagi <fdu41039@kwansei.ac.jp>
2017-12-28 15:00:15 +0900

    update exe dirs

--- lib/my_help_thor.rb
+++ lib/my_help_thor.rb
@@ -119,8 +119,9 @@ EOS
     map "--make" => "make"
     def make
       local_help_entries.each{|file|
+        help_name=File.basename(file,".yml")
         exe_cont="#!/usr/bin/env ruby\nrequire 'specific_help_opt'\nrequire 'specific_help_thor'\n"
-        exe_cont << "ENV['HELP_NAME']='emacs_help'\n"
+        exe_cont << "ENV['HELP_NAME']='#{help_name}'\n"
         exe_cont << "help_file = File.join(ENV['HOME'],'.my_help','#{file}')\n"
         exe_cont << "SpecificHelp::Command.start(ARGV)\n"
         exe_cont << "SpecificHelpOpt::Command.run(help_file, ARGV)\n"
