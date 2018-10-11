9d27e8c6e766b21c4b8a5852343e615c114444f5
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2018-10-11 14:04:46 +0900

    add tomo_help...

--- lib/my_help.rb
+++ lib/my_help.rb
@@ -1,5 +1,6 @@
 require 'my_help/version.rb'
 require 'my_help/my_help_controll'
+require 'my_help/tomo_help_controll'
 require 'my_help/org2yml'
 require 'colorize'
 # Add requires for other files you add to your project here, so
