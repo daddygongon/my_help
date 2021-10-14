128b9f4bb603c6fffe8434add36ab63ae8589098
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2021-10-14 13:20:37 +0900

    start Unit::test

--- lib/my_help.rb
+++ lib/my_help.rb
@@ -1,123 +1,124 @@
+require 'thor'
+require 'command_line/global'
+require 'colorize'
+
 require 'my_help/version.rb'
 require 'my_help/my_help_controll'
 require 'my_help/tomo_help_controll'
 require 'my_help/org2yml'
-require 'colorize'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
 
-
-require 'thor'
-require 'command_line/global'
-
-class MyCLI < Thor
-  desc('setup', 'set up the test database')
-  def setup(*args)
-    $control = MyHelp::Control.new()
-  end
-
-  desc "list [HELP] [ITEM]", "list all helps, specific HELP, or ITEM"
-  def list(*args)
-    invoke :setup
-    file = args[0]
-    item = args[1]
-    if file.nil?
-      puts $control.list_all.blue # list []
-    elsif item.nil?
-      begin
-        puts $control.list_help(file) # list [file]
-      rescue => e
-        puts e.to_s.red
-      end
-    else
-      begin
-        puts $control.show_item(file, item) # list [file] [item]
-      rescue => e
-        puts e.to_s.red
-      end
+module MyHelp
+  class CLI < Thor
+    desc('setup', 'set up the test database')
+    def setup(*args)
+      $control = MyHelp::Control.new()
     end
-  end
 
-  desc "version", "show version"
-  def version
-    invoke :setup
-    puts MyHelp::VERSION
-  end
-
-  desc "set_editor EDITOR_NAME", "set editor to EDITOR_NAME"
-  def set_editor(editor_name)
-    invoke :setup
-    $control.set_editor(editor_name)
-  end
-
-  desc "edit HELP", "edit HELP"
-  def edit(help_name)
-    invoke :setup
-    $control.edit_help(help_name)
-  end
-
-  desc "new HELP", "make new HELP"
-  def new(help_name)
-    invoke :setup
-    $control.init_help(help_name)
-  end
-
-  desc "delete HELP", "delete HELP"
-  def delete(help_name)
-    invoke :setup
-    $control.delete_help(help_name)
-  end
-
-  desc "git [push|pull]", "git push or pull"
-  def git(push_or_pull,*args)
-    p push_or_pull
-    invoke :setup
-    argument_size =  args.size
-    Dir.chdir($control.local_help_dir) do
-      case push_or_pull
-      when 'push'
-        if argument_size == 0
-          comms = ['git add -A',
-                   "git commit -m 'git push from my_help'",
-                   "git push origin main"]
-        else
-          p args
-          argument_size.times do |i|
-            orgfile = args[i] + '.org'
-            file = File.join($control.local_help_dir,orgfile)
-            if File.exist?(file) == true
-              puts orgfile.green
-              dir = $control.local_help_dir
-              Dir.chdir(dir) do
-                comm = 'git add ' + file
-                puts c
-                c = command_line(comm)
-                puts c.stdout.blue
-                puts c.stderr.red
-              end
-            else
-              puts (orgfile + " does not existed").red
-            end
-          end
-          comms = ["git commit -m 'git push from my_help'",
-                   "git push origin main"]
+    desc "list [HELP] [ITEM]", "list all helps, specific HELP, or ITEM"
+    def list(*args)
+      invoke :setup
+      file = args[0]
+      item = args[1]
+      if file.nil?
+        puts $control.list_all.blue # list []
+      elsif item.nil?
+        begin
+          puts $control.list_help(file) # list [file]
+        rescue => e
+          puts e.to_s.red
         end
-      when 'pull'
-        comms = ['git pull']
       else
-        raise "my_help git was called by the other than 'push or pull'"
-      end
-      comms.each do |comm|
-        puts comm
-        c = command_line(comm)
-        puts c.stdout.blue
-        puts c.stderr.red
+        begin
+          puts $control.show_item(file, item) # list [file] [item]
+        rescue => e
+          puts e.to_s.red
+        end
       end
     end
+
+    desc "version", "show version"
+    def version
+      invoke :setup
+      puts VERSION
+    end
+
+    desc "set_editor EDITOR_NAME", "set editor to EDITOR_NAME"
+    def set_editor(editor_name)
+      invoke :setup
+      $control.set_editor(editor_name)
+    end
+
+    desc "edit HELP", "edit HELP"
+    def edit(help_name)
+      invoke :setup
+      $control.edit_help(help_name)
+    end
+
+    desc "new HELP", "make new HELP"
+    def new(help_name)
+      invoke :setup
+      $control.init_help(help_name)
+    end
+
+    desc "delete HELP", "delete HELP"
+    def delete(help_name)
+      invoke :setup
+      $control.delete_help(help_name)
+    end
+
+    desc "git [push|pull]", "git push or pull"
+    def git(push_or_pull,*args)
+      p push_or_pull
+      invoke :setup
+      argument_size =  args.size
+      Dir.chdir($control.local_help_dir) do
+        case push_or_pull
+        when 'push'
+          if argument_size == 0
+            comms = ['git add -A',
+                     "git commit -m 'git push from my_help'",
+                     "git push origin main"]
+          else
+            p args
+            argument_size.times do |i|
+              orgfile = args[i] + '.org'
+              file = File.join($control.local_help_dir,orgfile)
+              if File.exist?(file) == true
+                puts orgfile.green
+                dir = $control.local_help_dir
+                Dir.chdir(dir) do
+                  comm = 'git add ' + file
+                  puts c
+                  c = command_line(comm)
+                  puts c.stdout.blue
+                  puts c.stderr.red
+                end
+              else
+                puts (orgfile + " does not existed").red
+              end
+            end
+            comms = ["git commit -m 'git push from my_help'",
+                     "git push origin main"]
+          end
+        when 'pull'
+          comms = ['git pull']
+        else
+          raise "my_help git was called by the other than 'push or pull'"
+        end
+        comms.each do |comm|
+          puts comm
+          c = command_line(comm)
+          puts c.stdout.blue
+          puts c.stderr.red
+        end
+      end
+    end
+    #  desc "search {find_char}", "search FIND_CHAR"
+    #  def search(find_char)
+    #    invoke :setup
+    #    $control.search_help(find_char)
+    #  end
   end
-  #  desc "search {find_char}", "search FIND_CHAR"
-  #  def search(find_char)
-  #    invoke :setup
-  #    $control.search_help(find_char)
-  #  end
 end
