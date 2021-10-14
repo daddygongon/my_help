5f920807e5587749e35f9f80408548ba5e4bb976
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2021-10-14 13:20:37 +0900

    start Unit::test

--- test/my_help_test.rb
+++ test/my_help_test.rb
@@ -5,22 +5,15 @@ require "test_helper"
 require "tmpdir"
 require "fileutils"
 
-# Learner Test
-# Learner Test
-class LearnerTest < Test::Unit::TestCase
-  include Learner
+class MyHelpTest < Test::Unit::TestCase
+  include MyHelp
 
   test "VERSION" do
     assert do
-      ::Learner.const_defined?(:VERSION)
+      MyHelp.const_defined?(:VERSION)
     end
   end
 
-  test "Helloは\"Hello HOGEHOGE.\"を返す" do
-    assert_equal("Hello Rudy.", Hello.new.run("Rudy"))
-    assert_not_equal("Hello Rud.", Hello.new.run("Rudy"))
-    assert_equal("Hello world.", Hello.new.run)
-  end
 end
 
 
