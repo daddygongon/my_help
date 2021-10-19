edffeeca3fae37e96285b888b6fe065162245288
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2021-10-19 11:43:56 +0900

    rev lib/my_help/config.rb

--- test/my_help_test.rb
+++ test/my_help_test.rb
@@ -5,11 +5,16 @@ require "tmpdir"
 require "fileutils"
 
 class MyHelpTest < Test::Unit::TestCase
-  #  include MyHelp
+  include MyHelp
 
   test "VERSION" do
     assert do
       MyHelp.const_defined?(:VERSION)
+    end
+  end
+  test "Config" do
+    assert do
+      MyHelp::Config.new()
     end
   end
 end
