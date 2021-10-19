ddd060509ae9007e0bb49ae027b9125c783e874f
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2021-10-19 12:36:46 +0900

    rev lib/my_help/config.rb

--- test/my_help_test.rb
+++ test/my_help_test.rb
@@ -7,14 +7,19 @@ require "fileutils"
 class MyHelpTest < Test::Unit::TestCase
   include MyHelp
 
-  test "VERSION" do
-    assert do
-      MyHelp.const_defined?(:VERSION)
+  sub_test_case "Misc" do
+    test "VERSION" do
+      assert do
+        MyHelp.const_defined?(:VERSION)
+      end
     end
   end
-  test "Config" do
-    assert do
-      MyHelp::Config.new()
+  
+  sub_test_case "Config" do
+    test "initialize" do
+      assert do
+        p Config.new(Dir.pwd).config
+      end
     end
   end
 end
