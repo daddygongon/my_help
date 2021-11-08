13ae310f60d14cd405a90c12d9aeaddb50599308
Shigeto R. Nishitani <daddygongon@users.noreply.github.com>
2021-11-08 18:09:13 +0900

    using Config for test path

--- lib/my_help/config.rb
+++ lib/my_help/config.rb
@@ -1,50 +1,50 @@
+# frozen_string_literal: true
+
 require 'yaml'
 require 'colorize'
 
 module MyHelp
+  # make @config from default and load yaml
+  # as shown https://stackoverflow.com/questions/6233124/where-to-place-access-config-file-in-gem
   class Config
     # Configuration defaults
-    def initialize(conf_path=nil)
+    def initialize(conf_path = nil)
       @conf_path = conf_path || ENV['HOME']
       @config = {
-        :template_dir => File.expand_path("../../templates", __FILE__),
-        :local_help_dir => File.join(@conf_path, '.my_help'),
-        :conf_file => File.join(@conf_path, '.my_help', '.my_help_conf.yml'),
-        :editor => ENV['EDITOR'] || 'emacs'
+        template_dir: File.expand_path('../templates', __dir__),
+        local_help_dir: File.join(@conf_path, '.my_help'),
+        conf_file: File.join(@conf_path, '.my_help', '.my_help_conf.yml'),
+        editor: ENV['EDITOR'] || 'emacs'
       }
       @valid_config_keys = @config.keys
       configure_with(@config[:conf_file])
     end
 
-
     # Configure through hash
     def configure(opts = {})
-      opts.each do |k,v|
-        if @valid_config_keys.include? k.to_sym
-          @config[k.to_sym] = v
-        end
+      opts.each do |k, v|
+        @config[k.to_sym] = v if @valid_config_keys.include? k.to_sym
       end
     end
 
     # Configure through yaml file
     def configure_with(path_to_yaml_file)
       begin
-        config = YAML::load(IO.read(path_to_yaml_file))
+        config = YAML.safe_load(IO.read(path_to_yaml_file),
+                                permitted_classes: [Symbol])
       rescue Errno::ENOENT
         # log(:warning, "YAML configuration file couldn't be found. Using defaults.")
-        puts "YAML configuration file couldn't be found. Using defaults.".red
+        puts 'YAML configuration file couldn\'t be found. Using defaults.'.red
         return
       rescue Psych::SyntaxError
         # log(:warning, "YAML configuration file contains invalid syntax. Using defaults.")
-        puts "YAML configuration file contains invalid syntax. Using defaults.".red
+        puts 'YAML configuration file contains invalid syntax. Using defaults.'.red
         return
       end
 
       configure(config)
     end
 
-    def config
-      @config
-    end
+    attr_reader :config
   end
 end
--- lib/my_help/my_help_controll.rb
+++ lib/my_help/my_help_controll.rb
@@ -7,55 +7,46 @@ module MyHelp
   class Control
     attr_accessor :local_help_dir, :editor
     def initialize(conf_path=nil)
-      # for configuration setups
-      # see https://stackoverflow.com/questions/6233124/where-to-place-access-config-file-in-gem
       @conf_path = conf_path || ENV['HOME']
       @conf = Config.new(@conf_path).config
-      puts YAML.dump(@conf)
-      @template_dir = @conf[:template_dir] #File.expand_path("../../templates", __FILE__)
-      @local_help_dir = @conf[:local_help_dir] #File.join(@conf_path, '.my_help')
-      @conf_file = @conf[:conf_file] # File.join(@local_help_dir, '.my_help_conf.yml')
-      @editor = @conf[:editor] #ENV['EDITOR'] || 'emacs' #'code', 'emacs' #'vim' #default editor
-      # @mini_account = File
+      # puts YAML::dump(@conf)
       set_help_dir_if_not_exists
       load_conf
     end
 
     def load_conf
       begin
-        conf = YAML.load_file(@conf_file)
+        conf = YAML.load_file(@conf[:conf_file])
         @editor = conf[:editor]
       rescue => e
         puts e.to_s.red
         puts 'make .my_help_conf.yml'.green
-        set_editor(@editor)
+        set_editor(@conf[:editor])
       end
     end
 
     def set_editor(editor)
-      @editor = editor
-#      file_name = '.my_help_conf.yml'
-#      @conf_file = File.join(@local_help_dir, file_name)
+      @conf[:editor] = editor
       conf = {editor: editor}
-      File.open(@conf_file, 'w'){|f| YAML.dump(conf, f)}
-      puts "set editor '#{@editor}'"
+      File.open(@conf[:conf_file], 'w'){|f| YAML.dump(conf, f)}
+      puts "set editor '#{@conf[:editor]}'"
     end
 
     def set_help_dir_if_not_exists
-      return if File::exist?(@local_help_dir)
-      FileUtils.mkdir_p(@local_help_dir, :verbose=>true)
-      Dir.entries(@template_dir).each{|file|
+      return if File::exist?(@conf[:local_help_dir])
+      FileUtils.mkdir_p(@conf[:local_help_dir], :verbose=>true)
+      Dir.entries(@conf[:template_dir]).each{|file|
         next if file=='help_template.org'
-        file_path=File.join(@local_help_dir,file)
+        file_path=File.join(@conf[:local_help_dir],file)
         next if File::exists?(file_path)
-        FileUtils.cp((File.join(@template_dir,file)),@local_help_dir,:verbose=>true)
+        FileUtils.cp((File.join(@conf[:template_dir],file)),@conf[:local_help_dir],:verbose=>true)
       }
     end
 
     def list_all
       output = "\nList all helps\n"
       local_help_entries.each do |file|
-        file_path=File.join(@local_help_dir,file)
+        file_path=File.join(@conf[:local_help_dir],file)
         title = file.split('.')[0]
         help = auto_load(file_path)
         next if help.nil?
@@ -75,10 +66,10 @@ module MyHelp
     WrongFileName = Class.new(RuntimeError)
     def list_help(file)
       output = ''
-      file_path=File.join(@local_help_dir,file+'.org')
+      file_path=File.join(@conf[:local_help_dir],file+'.org')
       begin
         help = auto_load(file_path)
-      rescue => e
+      rescue
         raise WrongFileName, "No help named '#{file}' in the directory '#{local_help_dir}'."
       end
       help.each_pair do |key, conts|
@@ -91,7 +82,7 @@ module MyHelp
     WrongItemName = Class.new(RuntimeError)
     def show_item(file, item)
       output = ''
-      file_path=File.join(@local_help_dir,file+'.org')
+      file_path=File.join(@conf[:local_help_dir],file+'.org')
       help = auto_load(file_path)
       select = select_item(help, item)
       output << help[:head][:cont]
@@ -103,11 +94,11 @@ module MyHelp
     end
 
     def edit_help(file)
-      p target_help = File.join(@local_help_dir,file+'.org')
+      p target_help = File.join(@conf[:local_help_dir],file+'.org')
       if local_help_entries.member?(file+'.org')
-        system "#{@editor} #{target_help}"
+        system "#{@conf[:editor]} #{target_help}"
       else
-        puts "file #{target_help} does not exits in #{@local_help_dir}."
+        puts "file #{target_help} does not exits in #{@conf[:local_help_dir]}."
         puts "init #{file} first."
       end
     end
@@ -117,106 +108,93 @@ module MyHelp
         puts "specify NAME".red
         exit
       end
-      p target_help=File.join(@local_help_dir,file+'.org')
+      p target_help=File.join(@conf[:local_help_dir],file+'.org')
       if File::exists?(target_help)
         puts "File exists. delete it first to initialize it."
         exit
       end
-      p template = File.join(@template_dir,'help_template.org')
+      p template = File.join(@conf[:template_dir],'help_template.org')
       FileUtils::Verbose.cp(template,target_help)
     end
 
-=begin
-       def delete_help(file)
-         file = File.join(@local_help_dir,file+'.org')
-         print "Are you sure to delete "+file.blue+"?[Ynq] ".red
-         case STDIN.gets.chomp
-         when 'Y'
-           begin
-             FileUtils.rm(file,:verbose=>true)
-           rescue => error
-             puts error.to_s.red
-           end
-         when 'n', 'q' ; exit
-         end
-       end
-=end
-     def delete_help(file)
-       file = File.join(@local_help_dir,file+'.org')
-       if File.exist?(file) == true
-         print "Are you sure to delete "+file.blue+"?[Yn] ".red
-         case STDIN.gets.chomp
-         when 'Y'
-           begin
-             FileUtils.rm(file,:verbose=>true)
-             return 0
-           rescue => error
-             puts error.to_s.red
-             return 1
-           end
-         when 'n', 'q' ; return 0
-         end
-       else
-         print file + " is a non-existent file"
-       end
-     end
+    def delete_help(file)
+      file = File.join(@conf[:local_help_dir],file+'.org')
+      if File.exist?(file) == true
+        print "Are you sure to delete "+file.blue+"?[Yn] ".red
+        case STDIN.gets.chomp
+        when 'Y'
+          begin
+            FileUtils.rm(file,:verbose=>true)
+            return 0
+          rescue => error
+            puts error.to_s.red
+            return 1
+          end
+        when 'n', 'q' ; return 0
+        end
+      else
+        print file + " is a non-existent file"
+      end
+    end
 
-     def search_help(word)
-       p find_char = word
-       system "ls #{@local_help_dir} | grep #{find_char}"
-     end
+    def search_help(word)
+      p find_char = word
+      system "ls #{@conf[:local_help_dir]} | grep #{find_char}"
+    end
 
-     private
-     def select_item(help, item)
-       o_key = nil
-       help.each_pair do |key, cont|
-         next if key==:license or key==:head
-         if cont[:opts][:short] == item or cont[:opts][:long] == item
-           o_key = key
-           break
-         end
-       end
-       o_key
-     end
+    private
+    def select_item(help, item)
+      o_key = nil
+      help.each_pair do |key, cont|
+        next if key==:license or key==:head
+        if cont[:opts][:short] == item or cont[:opts][:long] == item
+          o_key = key
+          break
+        end
+      end
+      o_key
+    end
 
-     def disp_opts( conts )
-       output = ''
-       col = 0
-       conts.each_pair do |key, item|
-         col_length = case col
-                      when 0; output << item.rjust(5)+", "
-                      when 1; output << item.ljust(15)+": "
-                      else; output << item
-                      end
-         col += 1
-       end
-       output << "\n"
-     end
-     def local_help_entries
-       entries= []
-       Dir.entries(@local_help_dir).each{|file|
-         #        next unless file.include?('_')
-         next if file[0]=='#' or file[-1]=='~' or file[0]=='.'
-         #        next if file.match(/(.+)_e\.org/) # OK?
-         #        next if file.match(/(.+)\.html/)
-         if file.match(/(.+)\.org$/) # OK?
-           entries << file
-         end
-       }
-       return entries
-     end
+    def disp_opts( conts )
+      output = ''
+      col = 0
+      conts.each_pair do |key, item|
+        case col
+        when 0 ; output << item.rjust(5)+", "
+        when 1 ; output << item.ljust(15)+": "
+        else   ; output << item
+        end
+        col += 1
+      end
+      output << "\n"
+    end
 
-     def auto_load(file_path)
-       case File.extname(file_path)
-         #      when '.yml'
-         #        cont = YAML.load(File.read(file_path))
-       when '.org'
-         cont = OrgToYaml.new(file_path).help_cont
-       else
-         puts "Not handling file types of #{file_path}"
-         cont = nil
-       end
-       cont
-     end
-   end
- end
+    def local_help_entries
+      entries= []
+      Dir.entries(@conf[:local_help_dir]).each{|file|
+        #        next unless file.include?('_')
+        next if file[0]=='#' or file[-1]=='~' or file[0]=='.'
+        #        next if file.match(/(.+)_e\.org/) # OK?
+        #        next if file.match(/(.+)\.html/)
+        if file.match(/(.+)\.org$/) # OK?
+          entries << file
+        end
+      }
+      return entries
+    end
+
+    def auto_load(file_path)
+      case File.extname(file_path)
+        #      when '.yml'
+        #        cont = YAML.load(File.read(file_path))
+      when '.org'
+        cont = OrgToYaml.new(file_path).help_cont
+      else
+        puts "Not handling file types of #{file_path}"
+        cont = nil
+      end
+      cont
+    end
+  end
+end
+
