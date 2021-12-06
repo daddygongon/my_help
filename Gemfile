bcdc686849b97c632140d4fdf4d90431208055bb
Moeko Higashibata <ewh42930@kwansei.ac.jp>
2021-12-06 20:11:48 +0900

    new file:   spec/my_help_spec.rb

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gemspec
 gem 'ffi', '~> 1.9'
-gem 'aruba', '~> 0.14.12'
+#gem 'aruba', '~> 0.14.12'
 gem 'command_line', '~> 1.1.0'
 gem 'color_echo'
+#gem 'aruba', '~> 2.0.0'
---
+++ spec/my_help_spec.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+RSpec.describe MyHelp do
+  include MyHelp
+  it "has a version number" do
+    expect(MyHelp::VERSION).not_to be nil
+  end
+
+  describe "MyHelp::SetEditor" do
+    subject(:set_editor) {MyHelp::SetEditor.new}
+
+    describe "run with an arg 'moeko'" do
+      it {expect(set_editor.start('moeko')).to eq('my_help called with moeko
+        set editor moeko')}
+    end
+
+  end
+
+  
+end
