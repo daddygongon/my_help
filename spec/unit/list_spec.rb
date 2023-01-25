e8a2085c01e51098efbc35ff5cb2bf2b10265223
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2023-01-25 11:56:20 +0900

    rev list_spec.rb

--- spec/unit/list_spec.rb
+++ spec/unit/list_spec.rb
@@ -1,40 +1,40 @@
 module MyHelp
   RSpec.describe List do
-    let(:templates_path) {
+    let(:templates_path) do
       File.join(File.dirname(__FILE__),
-                "../../lib/templates")
-    }
+                '../../lib/templates')
+    end
 
-    describe "list" do
-      it "ヘルプ名がないときは，全てのヘルプとその簡単な内容説明を表示" do
-        output = "example: "
-        #p List.new(templates_path).list
+    describe 'list' do
+      it 'ヘルプ名がないときは，全てのヘルプとその簡単な内容説明を表示' do
+        output = 'example: '
+        # p List.new(templates_path).list
         expect(List.new(templates_path).list).to be_include(output)
       end
-      it "ヘルプ名があるときは，その中の全てのitemを表示" do
-        output = "head"
-        help_options = "example"
+      it 'ヘルプ名があるときは，その中の全てのitemを表示' do
+        output = 'example'
+        help_options = 'example'
         p List.new(templates_path).list(help_options)
         expect(List.new(templates_path).list(help_options)).to be_include(output)
       end
-      it "item名があるときは，そのcontentを表示" do
-        output = "- content_b"
-        help_options = "example b_item"
+      it 'item名があるときは，そのcontentを表示' do
+        output = '- content_b'
+        help_options = 'example b_item'
         expect(List.new(templates_path).list(help_options)).to be_include(output)
       end
-      it "item名に似たitemがあるときは，そのcontentを表示" do
-        output = "- content_b"
-        help_options = "example b_"
+      it 'item名に似たitemがあるときは，そのcontentを表示' do
+        output = '- content_b'
+        help_options = 'example b_'
         expect(List.new(templates_path).list(help_options)).to be_include(output)
       end
-      it "item名に似たitemが見つからないときは，その旨を返す" do
+      it 'item名に似たitemが見つからないときは，その旨を返す' do
         output = "Can't find similar item name with :"
-        help_options = "example ba_item"
+        help_options = 'example ba_item'
         expect(List.new(templates_path).list(help_options)).to be_include(output)
       end
     end
-    describe "md extension" do
-      it "拡張子がmdならそっちで動く"
+    describe 'md extension' do
+      it '拡張子がmdならそっちで動く'
     end
   end
 end
